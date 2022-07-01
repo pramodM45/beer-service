d346fa28d06469cbeadb56b8f7fe74c1e97ea868
pramodM45 <pramodkumarmukthineni@gmail.com>
2022-07-01 21:41:57 +0530

    adding model classes

---
+++ src/main/java/com/microservices/beerservice/web/model/BeerDto.java
@@ -0,0 +1,33 @@
+package com.microservices.beerservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BeerDto {
+    private UUID id;
+    private Integer version;
+
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastUpdatedDate;
+
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+
+    private Long upc;
+
+    private BigDecimal price;
+
+    private Integer quanitityOnHand;
+
+}
